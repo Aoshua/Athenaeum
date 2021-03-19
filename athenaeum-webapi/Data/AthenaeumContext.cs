82e86c6215042f8b35568f5214bcbca2bbf37226
Josh Abbott <64.josh@gmail.com>
2021-03-19 12:22:13 -0600

    Added context & 1 class

---
+++ athenaeum-webapi/Data/AthenaeumContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace athenaeum_webapi.Data
+{
+    public class AthenaeumContext : DbContext
+    {
+        public AthenaeumContext(DbContextOptions<AthenaeumContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<BookAuthor> BookAuthor { get; set; }
+        public DbSet<BookGenre> BookGenre { get; set; }
+        public DbSet<BookInCollection> BookInCollection { get; set; }
+        public DbSet<Collection> Collection { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Publisher> Publisher { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserBook> UserBook { get; set; }
+        public DbSet<UserCollection> UserCollection { get; set; }
+
+        public DbSet<view_BookInCollection_UserBook> view_BookInCollection_UserBook { get; set; }
+        public DbSet<view_BookInCollection_Publisher> view_BookInCollection_Publisher { get; set; }
+    }
+}
